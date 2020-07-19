a27952e5182330e46f5ffd29b5676991329bd145
Junya Kengo <kogepan6852@yahoo.co.jp>
2020-07-19 18:12:49 +0900

    P120までの解説

--- app/src/main/java/com/example/changetext/MainActivity.kt
+++ app/src/main/java/com/example/changetext/MainActivity.kt
@@ -8,13 +8,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button.setOnClickListener{
+        button.setOnClickListener {
             textView.text = "おやすみ"
         }
         button2.setOnClickListener {
             textView.text = button2.text
             println("aaaaaa")
         }
+
+        val dog: Dog = Dog()
+        dog.setName("ココア")
+        dog.setName("アイス")
+        println(dog.getName())
+
+        val cat = Cat("ココア")
+        println(cat.name)
+
+        fun String.sum(a: String, b:String) = "[" + a + b + "]"
+    }
+}
+
+class Cat(val name: String) {}
+
+class Dog {
+    private var name  = ""
+    public fun setName(name: String) {
+        this.name = name
+    }
+    fun getName() : String {
+        return name
     }
 }
 
