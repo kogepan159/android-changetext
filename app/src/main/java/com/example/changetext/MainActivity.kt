533d7a5cd2dec2cf744575ab0e8c48d31265a8ec
Junya Kengo <kogepan6852@yahoo.co.jp>
2020-07-25 09:34:22 +0900

    カウントアップ処理を作るヒント画面

--- app/src/main/java/com/example/changetext/MainActivity.kt
+++ app/src/main/java/com/example/changetext/MainActivity.kt
@@ -5,18 +5,20 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    var count: Int = 0
+    // ここにvarで値を作る(1)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button.setOnClickListener {
-            this.count = count + 1
-            textView.text = count.toString()
+            // 数字に+1をする(2)
+            // 変更後の値を、textViewに追加する
+
+            // Hint
+            // textView.text = count.toString() 数字をString型にする方法
         }
         button2.setOnClickListener {
-            this.count = count - 1
-            textView.text = count.toString()
-            println("aaaaaa")
+            // 数字に-1をする(3)
+            // 変更後の値を、textviewに追加する
         }
     }
 }
