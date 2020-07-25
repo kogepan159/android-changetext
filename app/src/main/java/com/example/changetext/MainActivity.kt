dfb696aff9c853ee8c1a981f374164446e6e9239
Junya Kengo <kogepan6852@yahoo.co.jp>
2020-07-25 09:30:09 +0900

    classを使わずに作成

--- app/src/main/java/com/example/changetext/MainActivity.kt
+++ app/src/main/java/com/example/changetext/MainActivity.kt
@@ -5,37 +5,18 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    val count:Count = Count()
+    var count: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button.setOnClickListener {
-            count.addSum()
-            textView.text = count.getSum().toString()
+            this.count = count + 1
+            textView.text = count.toString()
         }
         button2.setOnClickListener {
-            count.addMinasu()
-            textView.text = count.getSum().toString()
+            this.count = count - 1
+            textView.text = count.toString()
             println("aaaaaa")
         }
     }
 }
-
-class Count {
-    private var sum = 0
-
-    fun addSum() {
-        this.sum = this.sum + 1
-    }
-
-    fun addMinasu() {
-        this.sum = this.sum - 1
-    }
-
-
-    fun getSum(): Int {
-        return  this.sum
-    }
-
-}
-
