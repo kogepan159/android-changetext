0f82a0c2941e2d92ba2de47397be00170c898985
Junya Kengo <kogepan6852@yahoo.co.jp>
2020-07-26 07:59:43 +0900

    Revert "カウントアップ処理を作るヒント画面"
    This reverts commit 533d7a5cd2dec2cf744575ab0e8c48d31265a8ec.

--- app/src/main/java/com/example/changetext/MainActivity.kt
+++ app/src/main/java/com/example/changetext/MainActivity.kt
@@ -5,20 +5,18 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    // ここにvarで値を作る(1)
+    var count: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button.setOnClickListener {
-            // 数字に+1をする(2)
-            // 変更後の値を、textViewに追加する
-
-            // Hint
-            // textView.text = count.toString() 数字をString型にする方法
+            this.count = count + 1
+            textView.text = count.toString()
         }
         button2.setOnClickListener {
-            // 数字に-1をする(3)
-            // 変更後の値を、textviewに追加する
+            this.count = count - 1
+            textView.text = count.toString()
+            println("aaaaaa")
         }
     }
 }
